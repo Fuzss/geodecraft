6b4116dd744ff87664619208204ea51f1a3e8679
Fuzss <28218241+Fuzss@users.noreply.github.com>
2026-08-20 12:44:50 +0200

    clear main

--- Common/src/main/java/net/yeoxuhang/geode_plus/util/BooleanFunction.java
+++
@@ -1,6 +0,0 @@
-package net.yeoxuhang.geode_plus.util;
-
-@FunctionalInterface
-public interface BooleanFunction<R> {
-    R apply(boolean var1);
-}
--- Forge/src/main/java/net/yeoxuhang/geode_plus/ForgeCommonProxy.java
+++
@@ -1,7 +0,0 @@
-package net.yeoxuhang.geode_plus;
-
-public class ForgeCommonProxy {
-    public void clientSetUp(){}
-
-    public void serverSetUp(){}
-}
